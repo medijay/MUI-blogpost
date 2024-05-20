ff3579c72d7329e7315302ae6843a5166434fbee
Ahmed Adekunle <medijay99@yahoo.com>
2024-05-20 13:57:21 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import XIcon  from '@mui/icons-material/X';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import Header from './components/Header'
+import Sidebar from './components/Sidebar';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import post1 from './components/blog-post1.md';
+import post2 from './components/blog-post2.md';
+import post3 from './components/blog-post3.md';
 
-function App() {
+
+const sidebar = {
+  title: 'About',
+  description:
+  'Ahmed Adekunle aka mr medijay blogging website',
+  archives: [
+    {title: 'March 2020', url: '#'},
+    {title: 'February 2020', url: '#'},
+    {title: 'January 2020', url: '#'},
+    {title: 'November 1999', url: '#'},
+    {title: 'October 1999', url: '#'},
+    {title: 'September 1999', url: '#'},
+    {title: 'August 1999', url: '#'},
+    {title: 'July 1999', url: '#'},
+    {title: 'June 1999', url: '#'},
+  ],
+  social : [
+    {name:'GitHub', icon:GitHubIcon},
+    {name:'X', icon:XIcon},
+    {name:'Facebook', icon: FacebookIcon },
+  ],
+};
+
+const posts = [post1, post2, post3];
+
+const sections = [
+   { title: 'Technology', url: '#'},
+   { title: 'Markets', url: '#'},
+   { title: 'Culture', url: '#'},
+   { title: 'Business', url: '#'},
+   { title: 'Politics', url: '#'},
+   { title: 'Policy', url: '#'},
+   { title: 'Opinion', url: '#'},
+   { title: 'Health', url: '#'},
+   { title: 'Travel', url: '#'},
+];
+
+
+
+const defaultTheme = createTheme();
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <ThemeProvider theme={defaultTheme} >
+    <CssBaseline />
+    <Container maxwidth="lg">
+    <Header title="Blog" sections={sections} />
 
-export default App;
+    <Grid container spacing={5} sx={{ mt:3 }}>
+      <Main title="From the firehouse" posts={posts} />
+      <Sidebar
+      title={sidebar.title}
+      description={sidebar.description}
+      archives={sidebar.archives}
+      social={sidebar.social}
+      />
+    </Grid>
+      </Container>
+      <Footer
+      title="Mj Inc"
+      description="Designed and created TrukSaint!!!!!"
+      />
+    </ThemeProvider>
+  );
+};
+
---
+++ src/components/blog-post1.md
@@ -0,0 +1,17 @@
+Bitcoin Falls. What's Holding Back a Crypto Rally. — Barrons.com
+May 20, 202410:53 GMT+1
+By Adam Clark
+
+Bitcoin and other cryptocurrencies were falling Monday, with the crypto sector's momentum still highly dependent on flows into exchange-traded funds.
+
+Bitcoin has fallen 0.6% over the last 24 hours to $66,860. The largest cryptocurrency hit a record high near $74,000 in mid-March amid a surge of interest from new spot Bitcoin exchange-traded funds (ETFs) but its price has dropped since then.
+
+"A new impetus is needed in purchases of ETFs to revitalize demand growth. Likewise, the growth of liquidity in stablecoins is still slow. A sustainable price rally is usually associated with an increase in stablecoin liquidity, which hasn't yet materialized," wrote Javier Molina, a market analyst at eToro, in a research note.
+
+However, there was good news elsewhere in the crypto sector after a judge approved a wind-down plan for Genesis Global on Friday. The plan will pay back creditors about 77% of the same digital assets that they loaned to the cryptocurrency lender before its bankruptcy filing nearly 16 months ago.
+
+Beyond Bitcoin, Ether — the second-largest crypto — was down 0.7% to $3,091. Smaller cryptos or altcoins also fell, with Cardano down 2.8% and Polygon dropping 2.3%. Dogecoin fell 1.8%.
+
+Write to Adam Clark at adam.clark@barrons.com
+
+This content was created by Barron's, which is operated by Dow Jones & Co. Barron's is published independently from Dow Jones Newswires and The Wall Street Journal.
