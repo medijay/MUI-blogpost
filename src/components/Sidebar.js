78ba2b823be89b22dc91286aeebc0d8df63b7eb0
Ahmed Adekunle <medijay99@yahoo.com>
2024-07-20 05:25:20 +0100

    removed socials from sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,12 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import Grid from "@mui/material/Grid";
-import Stack  from "@mui/material/Stack";
 import Paper from "@mui/material/Paper";
 import Typography  from "@mui/material/Typography";
 import Link  from "@mui/material/Link";
 
 function Sidebar(props) {
-    const {archives, description, social, title } = props;
+    const {archives, description,title } = props;
 
     return (
         <Grid item xs={12} md={4}>
@@ -25,24 +24,6 @@ function Sidebar(props) {
                     {archive.title}
                 </Link>
             ))}
-            <Typography variant="h6" gutterBottom sx={{ mt: 3}}>
-                Social
-            </Typography>
-            {social.map((network) => (
-                <Link
-                display="block"
-                variant="body1"
-                href="#"
-                key={network.name}
-                sx={{ mb: 0.5}}
-                >
-                    <Stack direction="row" spacing={1} alignItems="center">
-                        <network.icon />
-                        <span>{network.name}</span>
-                    </Stack>
-                </Link>
-            ))}
-            
                 </Grid>
                 );
             }
