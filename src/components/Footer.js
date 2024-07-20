2dfa291d810070126f0c8a0b65742e3ab98ceabc
Ahmed Adekunle <medijay99@yahoo.com>
2024-07-20 05:24:13 +0100

    add socials to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,15 @@ import { Box } from '@mui/material';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import {Stack} from '@mui/material';
 
-
-function Copyright() {
+  
+  function Footer(props) {
+    const { description, title, social } = props;
 
     return (
+          <Box component="footer" sx={{ bgcolor: '#212121', py: 2}}>
+
         <Typography variant="body2" color="#fafafa" align="center">
           {'Copyright © '}
           <Link color="#fafafa" href="https://medijay.github.io">
@@ -17,14 +21,7 @@ function Copyright() {
           {new Date().getFullYear()}
           {'.'}
         </Typography>
-      );
-    }
-    
-    function Footer(props) {
-      const { description, title } = props;
-    
-      return (
-        <Box component="footer" sx={{ bgcolor: '#212121', py: 6 }}>
+      
           <Container maxWidth="lg">
             <Typography  color="#fafafa" variant="h5" align="center" gutterBottom>
               {title}
@@ -34,18 +31,47 @@ function Copyright() {
               align="center"
               color="#fafafa"
               component="p"
-            >
+              >
               {description}
             </Typography>
-            <Copyright />
           </Container>
+
+    
+          <Typography>
+            <Stack direction={'row'} spacing={2} justifyContent={'flex-end'}>
+
+
+            {social.map((network) => (
+              <Link
+              display="grid"
+              variant="body1"
+              href="#"
+              key={network.name}
+              sx={{ mb: 0.5, }}
+              >
+                    <Stack direction="row" spacing={1} alignItems="center" color={'white'} >
+                        <network.icon />
+                    </Stack>
+                </Link>
+            ))}
+            </Stack>
+            </Typography>
+        
         </Box>
       );
-    };
+      
+    }
 
     Footer.prototype = {
         description: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired,
+
+                social: PropTypes.arrayOf(
+                    PropTypes.shape({
+                        icon: PropTypes.elementType,
+                        name: PropTypes.string.isRequired,
+                    })
+                  ),
     };
 
     export default Footer;
