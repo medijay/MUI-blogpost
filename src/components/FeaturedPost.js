44c5d0be2e40cddbf4ff3f40252d123bf01c6af2
Ahmed Adekunle <medijay99@yahoo.com>
2024-08-27 16:26:51 +0100

    recent commit

--- src/components/FeaturedPost.js
+++ src/components/FeaturedPost.js
@@ -9,45 +9,26 @@ import CardMedia from "@mui/material/CardMedia";
 
 
 function FeaturedPost() {
-    
-    const featuredPosts = [
-        {
-          title: 'Featured post',
-          date: 'Nov 12',
-          description:
-            'This is a wider card with supporting text below as a natural lead-in to additional content.',
-          image: 'https://source.unsplash.com/random?wallpapers',
-          imageLabel: 'Image Text',
-        },
-        {
-          title: 'Post title',
-          date: 'Nov 11',
-          description:
-            'This is a wider card with supporting text below as a natural lead-in to additional content.',
-          imageLabel: 'Image Text',
-        },
-      ];
 
     return (
         <Grid item xs={12} md={6}>
             <CardActionArea component="a" href="#">
                 <Card sx={{ display:'flex' }}>
                     <CardContent sx={{ flex:1 }}>
-                        {featuredPosts.map((post) =>(
-                         <FeaturedPost   key={post.title} post={post} />
-                        )
+                        
+                         <FeaturedPost    />
+                    
                         
                            
-                        )}
+                
                         <Typography component="h2" variant="h5">
-                            {featuredPosts.title}
                         </Typography>
 
                         <Typography variant="subtitle1" color="text.secondary">
-                            {featuredPosts.date}
+    
                         </Typography>
                         <Typography variant="subtitle1" paragraph>
-                            {featuredPosts.description1}
+                        
                         </Typography>
                 
                         <Typography variant="subtitle1" color="primary">
@@ -58,8 +39,8 @@ function FeaturedPost() {
                     <CardMedia
                     component="img"
                     sx={{ width:160, display: {xs: 'none', sm: 'block'} }}
-                    image={featuredPosts.image}
-                    alt={featuredPosts.imageLabel}
+                    
+    
                     />
                 </Card>
             </CardActionArea>
