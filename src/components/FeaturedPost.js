41becef33bd08ffb3905a0d3b654053b1155e5b7
Ahmed Adekunle <medijay99@yahoo.com>
2024-08-31 10:32:42 +0100

    recent commit

--- src/components/FeaturedPost.js
+++ src/components/FeaturedPost.js
@@ -1,52 +1,36 @@
 import React from "react";
-import { Typography } from "@mui/material";
-import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import CardActionArea from "@mui/material/CardActionArea";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
 
 
 function FeaturedPost() {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
 
     return (
-        <Grid item xs={12} md={6}>
-            <CardActionArea component="a" href="#">
-                <Card sx={{ display:'flex' }}>
-                    <CardContent sx={{ flex:1 }}>
-                        
-                
-                    
-                        
-                           
-                
-                        <Typography component="h2" variant="h5">
-                        </Typography>
-
-                        <Typography variant="subtitle1" color="text.secondary">
-    
-                        </Typography>
-                        <Typography variant="subtitle1" paragraph>
-                        
-                        </Typography>
-                
-                        <Typography variant="subtitle1" color="primary">
-                            Continue reading...
-                        </Typography>
-                    
-                    </CardContent>
-                    <CardMedia
-                    component="img"
-                    sx={{ width:160, display: {xs: 'none', sm: 'block'} }}
-                    
-    
-                    />
-                </Card>
-            </CardActionArea>
-        </Grid>
+        <Box sx={{ maxWidth: { xs: 320, sm: 480 }, bgcolor: 'background.paper' }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+        >
+          <Tab label="Item One" />
+          <Tab label="Item Two" />
+          <Tab label="Item Three" />
+          <Tab label="Item Four" />
+          <Tab label="Item Five" />
+          <Tab label="Item Six" />
+          <Tab label="Item Seven" />
+        </Tabs>
+      </Box>
     );
-}
+  }
 
 
 export default FeaturedPost;
